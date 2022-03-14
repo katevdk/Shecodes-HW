987a9aa56c661619658df70193627e51fe8b91b4
katevdk <katevanderkley@gmail.com>
2022-03-14 12:30:50 +1100

    Fetch API data

--- weather.js
+++ weather.js
@@ -35,7 +35,8 @@ date.innerHTML =
   now.getFullYear();
 
 //
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector(".week");
   let forecastHTML = "";
   let days = ["Thru", "Fri", "Sat", "Sun"];
@@ -56,6 +57,10 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "fba31a6c985b0331975102e979ddcb33";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
 }
 
 // searched location
